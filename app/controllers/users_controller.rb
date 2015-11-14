5d0162f492b4a0e786537a973e99d0619a7762e1
Hiroko Fukushima <hiroko@Hiroko-no-MacBook-Air.local>
2015-11-14 12:51:04 +0900

    マイページshow画面作ってる

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    @nickname = current_user.nickname
+    @tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("create_at DESC")
+end
